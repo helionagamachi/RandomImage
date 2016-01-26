7ee7abfafa88c0550bf1a6ff5f8db04a8d7e0a0c
Hélio Nagamachi <helionagamachi@users.noreply.github.com>
2016-01-26 14:10:22 -0200

    Add the possibility to use different methods for image generation, using event to communicate between the two controllers

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ angular.module("randomImage", [])
     columns: 300,
     variance: 3,
     randomChance: 0.0001,
-    processing: false
+    processing: false,
+    method: "lines",
   }
   return state;
 })
-.controller("InputController", ["$scope", "state", function($scope, state){
+.factory('methods', function(){
+  return {
+    "lines": "line-worker",
+    "lines and cols": "line-and-col-worker"
+  }
+})
+.controller("InputController", ["$scope", "$rootScope", "state", "methods", function($scope, $rootScope, state, methods){
   $scope.state = state;
+
+  // $scope.$apply(function(){
+    $scope.state.methods = Object.keys(methods);
+  // })
+
   $scope.validateInt = function(key){
     $scope.state[key] = parseInt($scope.state[key]) || 0;
   }
@@ -18,29 +30,35 @@ angular.module("randomImage", [])
     $scope.state[key] = parseFloat($scope.state[key]) || 0.0;
   }
   $scope.start = function(){
-    $scope.state.processing = true;
+    if (!$scope.state.processing) {
+      $scope.state.processing = true;
+      $rootScope.$broadcast('start');
+    }
   }
+
+  $rootScope.$on('end', function(){
+    $scope.state.processing = false;
+  });
+
 }])
-.controller("CanvasController", ["$scope", "state", function($scope, state){
+.controller("CanvasController", ["$scope", "$rootScope", "state", "methods", function($scope, $rootScope, state, methods){
   // initialization
   $scope.state = state;
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
-  
-  $scope.$watch('state.processing', function(processing){
-    if(processing) {
-      var worker = new Worker("js/line-worker.js");
-      worker.onmessage = function(message) {
-        var messageType = message.data.type;
-        if (messageType == 'data'){
-          context.putImageData(message.data.imageData, message.data.x, message.data.y)
-        } else if(messageType == 'end') {
-          $scope.$apply(function(){
-            $scope.state.processing = false;
-          });
-        }
+
+  $rootScope.$on('start', function(event, args){
+    var workerName = methods[$scope.state.method];
+    var worker = new Worker("js/" + workerName + ".js");
+    worker.onmessage = function(message) {
+      var messageType = message.data.type;
+      if (messageType == 'data'){
+        context.putImageData(message.data.imageData, message.data.x, message.data.y);
+      } else if(messageType == 'end') {
+        $rootScope.$broadcast('end');
       }
-      worker.postMessage($scope.state);
     }
+    worker.postMessage($scope.state);
   });
+
 }]);
